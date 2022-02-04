30689a081adfcb54cd937907cf261817ab16ce60
Phellipe davyson <phellipedavyson@gmail.com>
2022-02-03 21:09:28 -0300

    refatoração

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:contato_id>', views.acessar_contato, name='acessar_contato'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Contact
 
 
 def index(request):
-    return render(request, 'contacts/index.html')
+    contatos = Contact.objects.all()
+    return render(request, 'contacts/index.html', {
+        'contatos': contatos
+    })
+
+
+def acessar_contato(request, contato_id):
+    contato = Contact.objects.get(id=contato_id)
+    return render(request, 'contacts/acessar_contato.html', {
+        'contato': contato
+    })
+
